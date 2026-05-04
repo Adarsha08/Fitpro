89339bf7b695b8f1da020afd384b0476e0af7c2d
Adarsha Gautam <gautamadarsha08@gmail.com>
2026-05-04 17:13:38 +0545

    fixed the login error

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -3,7 +3,8 @@ import SuperAdmin from "./(dashboard)/superAdmin/page";
 export default function Home() {
   return (
     <div className="h-screen" >
-      <SuperAdmin/>
+      {/* <SuperAdmin/> */}
+      <Login/>
     </div>
   );
 }
